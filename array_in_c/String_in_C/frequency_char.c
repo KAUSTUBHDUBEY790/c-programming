4443ab21c56bf03c4287a89f6a9bcf9c97208dd8
KAUSTUBHDUBEY790 <78341839+KAUSTUBHDUBEY790@users.noreply.github.com>
2021-02-01 12:46:58 +0530

    Update frequency_char.c
    Sir this was the program i was talking about in counting frequency of each character in string(Kaustubh Dubey, Section-c )

--- array_in_c/String_in_C/frequency_char.c
+++ array_in_c/String_in_C/frequency_char.c
@@ -1,45 +1,33 @@
-#include<string.h>
-// frequency of each char
 #include<stdio.h>
-
-void main()
+#include<string.h>
+int main()
 {
-    char st[100], reg[50];
-    int count = 0, i, j;
-
-    puts("Enter the String: ");
-    // scanf("%s", st); // single word 
-    //scanf("%[^\n]s", st); // single line
-    gets(st);  // single line
-    
-    int flag, index=0;
-    // logic here
-    for(i=0; st[i] != '\0'; i++)
-    {
-    flag = 1;
-    //
-    for(int k=0; k<index; k++)
-        if(st[i] == reg[k])
-        {
-            flag = 0;
-            break;
-        }    
-    if(flag)
-    {
-    reg[index++] = st[i];    
-    count = 0;
-    for(j=0; st[j] != '\0'; j++)
-        if(st[i] == st[j])
-            count++;
-            
-    
-    printf("char %c : %d\n", st[i], count);
-    }
-    }
-
-
-
-    
-
-
-}
+	char a[1000];
+        int i,n,count[1000],j=0,scount,f[1000];
+	printf("Enter the elements of the string ");
+        gets(a);
+	n=strlen(a);
+        for(i=0;a[i]!='\0';i++)
+	{
+		f[i]=1;
+  }
+        for(i=0;a[i]!='\0';i++)
+	{
+		scount=1;
+                for(j=i+1;a[j]!='\0';j++)
+		{
+                       if(a[i]==a[j])
+			{
+                       scount++;
+		       f[j]=0;
+                       }
+                }
+		if(f[i]!=0)
+                count[i]=scount;
+	}
+	for(i=0;a[i]!='\0';i++)
+	{
+	   if(f[i]!=0)	
+          printf("%c element appeared %d times\n",a[i],count[i]);
+	}
+	}
